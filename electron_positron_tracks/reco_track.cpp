0fc1c8c382aacd1a08635509f68a486832cb206d
Martin Vavrik <martinvitvavrik@gmail.com>
2022-08-29 12:01:50 +0200

    Spline energy reconstruction of both tracks.

--- electron_positron_tracks/reco_track.cpp
+++ electron_positron_tracks/reco_track.cpp
@@ -145,23 +145,73 @@ struct VectorField
     }
 };
 
+template<int nodes>
+TSpline3* FitSplines(TGraph* graph, const double min, const double max)
+{
+    double node_pos[nodes];
+    for (int i = 0; i < nodes; i++) node_pos[i] = min+(i/(nodes-1.0))*(max-min);
+
+    NSpline<nodes>* s = new NSpline<nodes>(node_pos);
+
+    TF1* fit = new TF1("zz_fit",s->GetEval(),min,max,2*nodes+2);
+    graph->Fit(fit,"M","",min,max);
+
+    double* fit_pars = fit->GetParameters();
+    double nodes_y[nodes];
+    for(int i = nodes; i < 2*nodes; i++) nodes_y[i-nodes]=fit_pars[i];
+    double beg1 = fit_pars[2*nodes];
+    double end1 = fit_pars[2*nodes+1];
+    
+    return new TSpline3("sp3",node_pos,nodes_y,nodes,"b1e1",beg1,end1);
+}
+
+void RecoEnergy(TSpline3* sp_fit, VectorField* magfield, TGraph* energy, TGraph* radius, TGraph* magnetic, double min, double max, double step)
+{
+    for (double x = min; x <= max; x += step)
+    {
+        int i_node = sp_fit->FindX(x);
+
+        double xnode,ynode,b,c,d;
+        sp_fit->GetCoeff(i_node,xnode,ynode,b,c,d);
+        double dx   = x-xnode;
+        double der  = b+dx*(2*c+3*d*dx);
+        double der2 = 2*c+6*d*dx;
+        double r = 0.01*pow(1+der*der,1.5)/der2;
+        const double clight = 299792458;
+        const double E0 = 510998.95;
+        Vector B = magfield->GetField(0,(8-sp_fit->Eval(x))/100,x/100);
+        double betasq = 1/(1+pow((E0/(clight*r*B.vx)),2));
+        double Ekin = E0*(1/sqrt(1-betasq)-1);
+        if (x > 4) energy->AddPoint(x,Ekin/1e6);
+        if (r < 1 && r > 0) radius->AddPoint(x,r*100);
+        magnetic->AddPoint(x,B.vx);
+    }
+}
+
 int reco_track()
 {
+    //file with Garfield simulation output
     TFile* inFile = new TFile("build/electrons.root");
     TTree* electrons = (TTree*)inFile->Get("electrons");
 
-    TCanvas* c1 = new TCanvas();
+    //plotting drift time vs distance to readout + linear fit
+    TCanvas* c_drift = new TCanvas("c_drift","Drift time");
     electrons->Draw("t1:8-y0","y1>7.0");
     TGraph* ty = new TGraph(electrons->GetSelectedRows(), electrons->GetV2(), electrons->GetV1());
+    ty->SetTitle("Drift time as function of distance;distance to readout [cm]; time [ns]");
+    ty->SetMarkerStyle(2);
+    ty->SetMarkerSize(0.4);
     ty->Draw("ap");
     ty->Fit("pol1","","",8.05,11);
 
+    //getting fit parameters
     TF1* ty_fit = (TF1*) ty->GetListOfFunctions()->FindObject("pol1");
     double a0 = ty_fit->GetParameter(0);
     double a1 = ty_fit->GetParameter(1);
     double b0 = -a0/a1; //inverse polynomial param
     double b1 = 1.0/a1; //inverse polynomial param
 
+    //setting variables from TTree
     double x0,y0,z0,t0,x1,y1,z1,t1;
     electrons->SetBranchAddress("x0",&x0);
     electrons->SetBranchAddress("y0",&y0);
@@ -172,6 +222,7 @@ int reco_track()
     electrons->SetBranchAddress("z1",&z1);
     electrons->SetBranchAddress("t1",&t1);
 
+    //zy (track) plot from original track and reconstructed from drif time
     TGraph* zy = new TGraph();
     TGraph* zy_reco = new TGraph();
 
@@ -185,7 +236,8 @@ int reco_track()
         }
     }
 
-    TCanvas* c2 = new TCanvas("c2","Electron track reconstruction");
+    //setting up track plots (original + reconstructed)
+    TCanvas* c_track = new TCanvas("c_track","Electron track reconstruction");
     
     zy_reco->SetTitle("Electron track reconstruction;z [cm]; distance to readout [cm]");
     zy_reco->SetMarkerStyle(2);
@@ -202,98 +254,62 @@ int reco_track()
     leg->AddEntry(zy_reco,"reconstructed");
     leg->Draw("same");
 
-    const int nodes = 10;
+    const int nodes = 25;
+    const int nodes2 = 5;
     double min = 0;
     double max = 15;
 
-    double node_pos[nodes];
-    for (int i = 0; i < nodes; i++) node_pos[i] = min+(i/(nodes-1.0))*(max-min);
+    //fitting both tracks with splines
+    TSpline3* sp_fit  = FitSplines<nodes>(zy,min,max);
+    TSpline3* sp_fit2 = FitSplines<nodes2>(zy_reco,min,max);
 
-    NSpline<nodes>* s = new NSpline<nodes>(node_pos);
-    //s->SetDer1(0);
-
-    TF1* zy_fit = new TF1("zz_fit",s->GetEval(),min,max,2*nodes+2);
-    zy->Fit(zy_fit,"M","",min,max);
-
-    double* fit_pars = zy_fit->GetParameters();
-    double nodes_y[nodes];
-    for(int i = nodes; i < 2*nodes; i++) nodes_y[i-nodes]=fit_pars[i];
-    double beg1 = fit_pars[2*nodes];
-    double end1 = fit_pars[2*nodes+1];
-    TSpline3 sp_fit("sp3",node_pos,nodes_y,nodes,"b1e1",beg1,end1);
-
-    TH1F* residues = new TH1F("h_residues","Residues",50,-0.5,0.5);
-
-    for (int i = 0; i < zy_reco->GetN(); i++)
-    {
-        double x,y;
-        zy_reco->GetPoint(i,x,y);
-        residues->Fill(y-zy_fit->Eval(x));
-    }
-
+    //loading magnetic field from txt file (units = meters)
     VectorField* magfield = new VectorField(-0.3,0.3,-0.3,0.3,-0.2,0.2,0.005);
     magfield->LoadField("/home/vavrik/work/X17/electron_positron_tracks/build/VecB.txt");
     
-    TGraph* energy_x = new TGraph();
-    TGraph* radius_x = new TGraph();
-    TGraph* magnetic_x = new TGraph();
+    TGraph* energy_x    = new TGraph();
+    TGraph* radius_x    = new TGraph();
+    TGraph* magnetic_x  = new TGraph();
+    TGraph* energy_x2   = new TGraph();
+    TGraph* radius_x2   = new TGraph();
+    TGraph* magnetic_x2 = new TGraph();
 
     double step = 0.1;
-    for (double x = min; x <= max; x += step)
-    {
-        int i_node = sp_fit.FindX(x);
-        double xnode,ynode,b,c,d;
-        sp_fit.GetCoeff(i_node,xnode,ynode,b,c,d);
-        double dx = x-xnode;
-        double der  = b+dx*(2*c+3*d*dx);
-        double der2 = 2*c+6*d*dx;
-        double r = 0.01*pow(1+der*der,1.5)/der2;
-        const double clight = 299792458;
-        const double E0 = 510998.95;
-        Vector B = magfield->GetField(0,(8-sp_fit.Eval(x))/100,x/100);
-        double betasq = 1/(1+pow((E0/(clight*r*B.vx)),2));
-        double Ekin = E0*(1/sqrt(1-betasq)-1);
-        if (x > 4) energy_x->AddPoint(x,Ekin/1e6);
-        if (r < 1 && r > 0) radius_x->AddPoint(x,r*100);
-        magnetic_x->AddPoint(x,B.vx);
-        //cout << "Kinetic energy at x = " << x << ": " << Ekin << " radius: " << r << "\n";
-        //cout << "comp: " << der << " retr: " << sp_fit.Derivative(x) << "\n";
-    }
+    RecoEnergy(sp_fit,magfield,energy_x,radius_x,magnetic_x,min,max,step);
+    RecoEnergy(sp_fit2,magfield,energy_x2,radius_x2,magnetic_x2,min,max,step);
 
     TCanvas* c_energy = new TCanvas("c_energy","Reconstructed energy");
     energy_x->SetTitle("Reconstructed energy;z [cm]; Energy [MeV]");
     energy_x->SetMarkerStyle(2);
     energy_x->SetMarkerSize(0.4);
+    energy_x->SetMarkerColor(2);
     energy_x->Draw("ap");
+
+    energy_x2->SetMarkerStyle(2);
+    energy_x2->SetMarkerSize(0.4);
+    energy_x2->Draw("p same");
 
     TCanvas* c_radius = new TCanvas("c_radius","Reconstructed radius");
     radius_x->SetTitle("Reconstructed radius;z [cm]; Radius [cm]");
     radius_x->SetMarkerStyle(2);
     radius_x->SetMarkerSize(0.4);
+    radius_x->SetMarkerColor(2);
     radius_x->Draw("ap");
+
+    radius_x2->SetMarkerStyle(2);
+    radius_x2->SetMarkerSize(0.4);
+    radius_x2->Draw("p same");
     
     TCanvas* c_magnetic = new TCanvas("c_magnetic","Perpendicular magnetic field");
     magnetic_x->SetTitle("Perpendicular magnetic field;z [cm]; B [T]");
     magnetic_x->SetMarkerStyle(2);
     magnetic_x->SetMarkerSize(0.4);
+    magnetic_x->SetMarkerColor(2);
     magnetic_x->Draw("ap");
 
-    double xbins = 121;
-    double zbins = 81;
-
-    TH2F* field = new TH2F("h_field","XZ magnetic field plot",xbins,-0.3,0.3,zbins,-0.2,0.2);
-
-    for(int i = 0; i < xbins; i++)
-        for(int j = 0; j < zbins; j++)
-        {
-            Vector b = magfield->GetField(-0.3+i*0.005,0.0,-0.2+j*0.005);//magfield->field[i][60][j];//
-            double bmag = sqrt(b.vx*b.vx+b.vy*b.vy+b.vz*b.vz);
-            field->SetBinContent(i,j,b.vz);
-        }
-    
-    TCanvas* c4 = new TCanvas("c4","Magnetic field XZ plane");
-    gStyle->SetPalette(1.);
-    field->Draw("colz");
+    magnetic_x2->SetMarkerStyle(2);
+    magnetic_x2->SetMarkerSize(0.4);
+    magnetic_x2->Draw("p same");
 
     return 0;
 }
@@ -342,3 +358,31 @@ int main()
     
     // TCanvas* c4 = new TCanvas("c4","Magnetic field XZ plane");
     // field->Draw("cont1");
+
+// magnetic field plot No. 2
+    // double xbins = 121;
+    // double zbins = 81;
+
+    // TH2F* field = new TH2F("h_field","XZ magnetic field plot",xbins,-0.3,0.3,zbins,-0.2,0.2);
+
+    // for(int i = 0; i < xbins; i++)
+    //     for(int j = 0; j < zbins; j++)
+    //     {
+    //         Vector b = magfield->GetField(-0.3+i*0.005,0.0,-0.2+j*0.005);//magfield->field[i][60][j];//
+    //         double bmag = sqrt(b.vx*b.vx+b.vy*b.vy+b.vz*b.vz);
+    //         field->SetBinContent(i,j,b.vz);
+    //     }
+    
+    // TCanvas* c4 = new TCanvas("c4","Magnetic field XZ plane");
+    // gStyle->SetPalette(1.);
+    // field->Draw("colz");
+
+// plot residues of reconstructed track
+    // TH1F* residues = new TH1F("h_residues","Residues",50,-0.5,0.5);
+
+    // for (int i = 0; i < zy_reco->GetN(); i++)
+    // {
+    //     double x,y;
+    //     zy_reco->GetPoint(i,x,y);
+    //     residues->Fill(y-sp_fit->Eval(x));
+    // }
