a8c9bf44e92fa645ad24a1be930272d0186f3a69
Vavrikus <martinvitvavrik@gmail.com>
2022-10-10 15:56:05 +0200

    Better distribution of works between individual jobs.

--- electron_positron_tracks/ion_electrons.cpp
+++ electron_positron_tracks/ion_electrons.cpp
@@ -26,55 +26,123 @@
 using namespace Garfield;
 using namespace std;
 
-//arguments: max_id, id, step (cm)
-int main(int argc, char * argv[])
+struct Job
 {
-    TApplication app("app", &argc, argv);
-
-    int max_id,id;
+    // most important parameters are jobs id, maximal id in given run
+    // and step in centimeters
+    int id, max_id;
     double step;
 
-    //check number of paramaters passed to main function
-    if (argc < 4)
+    // ranges for simulation
+    // 1st sector condition (x-sqrt(3)*z<=0)&&(x+sqrt(3)*z>0) has to be satisfied later
+    double xmin = -30;
+    double xmax = 30;
+    double ymin = -8;
+    double ymax = 8;
+    double zmin = 0;
+    double zmax = 15;
+
+    int nz,ny,nxz;     // variables for number of steps in y, z, x and z combined
+    int n_el;          // total number of electrons in all simulations
+    int min_el,max_el; // min/max indexes of electrons simulated in this simulation (inclusive)
+
+    double ysum;       // total distance to propagate all electrons
+
+    void GetParameters(int argc, char* argv[])
     {
-        cerr << "ERROR: Missing arguments in ion_electrons. Correct arguments: max_id, id, step (cm).\n";
-        return 1;
+        // check number of paramaters passed to main function
+        if (argc < 4)
+        {
+            cerr << "ERROR: Missing arguments in ion_electrons. Correct arguments: max_id, id, step (cm).\n";
+        }
+
+        // set parameters of program
+        max_id = stoi(argv[1]);
+        id     = stoi(argv[2]);
+        step   = stod(argv[3]);
+
+        if (id <= 0)     cerr << "ERROR: Parameter id has to be positive.\n";
+        if (id > max_id) cerr << "ERROR: Parameter id cannot be bigger than max_id.\n";
+        if (step <= 0.0) cerr << "ERROR: Parameter step has to be positive.\n";
+
+        cout << "Running with parameters:\n" << "max_id: " << max_id << ", id: " << id << ", step (cm): " << step << "\n";
     }
 
-    //set parameters of program
-    max_id = stoi(argv[1]);
-    id     = stoi(argv[2]);
-    step   = stod(argv[3]);
-
-    if (id <= 0) {cerr << "ERROR: Parameter id has to be positive.\n";return 2;}
-    if (id > max_id) {cerr << "ERROR: Parameter id cannot be bigger than max_id.\n"; return 3;}
-    cout << "Running with parameters:\n" << "max_id: " << max_id << ", id: " << id << ", step (cm): " << step << "\n";
-
-    //range for simulating electrons, 1st sector condition (x-sqrt(3)*z<=0)&&(x+sqrt(3)*z>0) has to be satisfied later
-    double xmin,xmax,ymin,ymax,zmin,zmax;
-    xmin = -30; xmax = 30; ymin = -8; ymax = 8; zmin = 0; zmax = 15;
-
-    //count the total number of electrons
-    int n_electrons = 0;
-    for (double x = xmin; x <= xmax; x+=step)
+    void SetElectronBounds()
     {
-        for (double y = ymin; y <= ymax; y+=step)
+        f_GetStepParameters();
+        n_el = ny*nxz;
+        ysum = (ymax-ymin)*nxz*ny-nxz*step*ny*(ny-1)/2;
+
+        cout << "Expecting " << n_el << " electrons across all simulations, total propagation distance " << ysum << ".\n";
+
+        min_el = f_Noptimal(id-1)+1;
+        max_el = f_Noptimal(id);
+
+        cout << "Simulating electrons from " << min_el << " to " << max_el << ", total propagation " << f_Ysum(max_el)-f_Ysum(min_el-1) << ".\n";
+    }
+
+private:
+    void f_GetStepParameters()
+    {
+        nz = floor((zmax-zmin)/step)+1; // number of z steps in each iteration
+        ny = floor((ymax-ymin)/step)+1; // number of y steps in each iteration
+        nxz = 0;                        // total number of x and z steps for each y step
+
+        // iterate over z and add up x steps to get nxz
+        for (int i = 0; i < nz; i++) 
         {
-            for (double z = zmin; z <= zmax; z+=step)
-            {
-                if((x-sqrt(3)*z<=0)&&(x+sqrt(3)*z>0)) n_electrons++;
-            }
+            double z     = zmin + i*step;                   // current z
+            int nxmin    = floor((-xmin-z*sqrt(3))/step)+1; // minimal number of x steps to satisfy minimal sector condition
+            double xmin2 = xmin + nxmin*step;               // smallest x to satisfy minimal sector condition
+
+            double nx; // number of x steps for current z iteration
+            if (xmin2 > (z*sqrt(3))) nx = 0; // if maximal sector condition cannot be satisfied
+            else nx = floor((z*sqrt(3)-xmin2)/step)+1;
+            nxz += nx;
         }
     }
 
-    int min_electron = ceil(((id-1)/(max_id*1.0))*n_electrons);  //minimal (exclusive) index of electron simulated in job with this id
-    int max_electron = ceil((id/(max_id*1.0))*n_electrons);      //maximal (inclusive) index of electron simulated in job with this id
+    // calculates total y to propagate for given number of electrons
+    double f_Ysum(int n_electrons)
+    {
+        int full_layers = floor(n_electrons/nxz); // full y layers
+        int remainder   = n_electrons%nxz;        // electrons in incomplete y layer on top
+        return (ymax-ymin)*nxz*full_layers - nxz*step*full_layers*(full_layers-1)/2 + remainder*(ymax-ymin-full_layers*step);
+    }
 
-    cout << "Expecting " << n_electrons << " electrons across all simulations.\n";
-    cout << "Simulating electrons from " << min_electron << " to " << max_electron << ".\n";
+    // finds best maximal index of electron for this id
+    int f_Noptimal(int l_id)
+    {
+        if (l_id == 0) return 0; // id 0 is not used but best minimal index of id 1 depends on this
+
+        int min_el = 0;
+        int max_el = ny*nxz;
+        double opt_ysum = ysum/max_id; // optimal total propagation for one job
+
+        int current_el = max_el/2;
+        while (min_el != current_el)
+        {
+            if(f_Ysum(current_el) > l_id*opt_ysum) max_el = current_el;
+            else min_el = current_el;
+            current_el = (max_el+min_el)/2; 
+        }  
+
+        return max_el;
+    }
+};
+
+//arguments: max_id, id, step (cm)
+int main(int argc, char* argv[])
+{
+    TApplication app("app", &argc, argv);
+
+    Job job;
+    job.GetParameters(argc,argv);
+    job.SetElectronBounds();
 
     //set output file, name dependent on id
-    string filename = "ion" + to_string(id) + ".root";
+    string filename = "ion" + to_string(job.id) + ".root";
 
     TFile outFile(filename.c_str(),"RECREATE","Electrons from ionization track");
     TTree electrons("electrons","Tree of initial and final points of secondary electrons");
@@ -112,7 +180,7 @@ int main(int argc, char * argv[])
     // Assemble a sensor.
     Sensor sensor;
     sensor.AddComponent(&grid); 
-    sensor.SetArea(xmin-space, ymin-space, xmin-space, xmax+space, ymax, zmax+space);
+    sensor.SetArea(job.xmin-space, job.ymin-space, job.xmin-space, job.xmax+space, job.ymax, job.zmax+space);
 
 
     //~ // We use microscopic tracking for simulating the electron avalanche.
@@ -130,18 +198,18 @@ int main(int argc, char * argv[])
     aval.EnableIonisationMarkers();
 
     int i = 0;
-    for (double x = xmin; x <= xmax; x+=step)
+    for (double y = job.ymin; y <= job.ymax; y+=job.step)
     {
-        for (double y = ymin; y <= ymax; y+=step)
+        for (double x = job.xmin; x <= job.xmax; x+=job.step)
         {
-            for (double z = zmin; z <= zmax; z+=step)
+            for (double z = job.zmin; z <= job.zmax; z+=job.step)
             {
                 //only inside 1 sector
                 if((x-sqrt(3)*z<=0)&&(x+sqrt(3)*z>0))
                 {
                     i++;
                     //if this electron is supposed to be simulated by job with this id
-                    if ((i > min_electron)&&(i<=max_electron))
+                    if (!((i < job.min_el)&&(i > job.max_el)))
                     {
                         int status;
                         aval.AvalancheElectron(x, y, z, 0, 0.1, 0, 0, 0);
