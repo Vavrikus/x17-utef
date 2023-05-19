3951ae2a41bcc0e4d6c98a2251e1c7960ebb8580
Vavrikus <martinvitvavrik@gmail.com>
2023-05-19 16:46:06 +0200

    Fixed rk_tracks.

--- LinkDef.h
+++ LinkDef.h
@@ -1,5 +1,6 @@
 #include "Field.h"
 #include "Points.h"
+#include "Track.h"
 
 namespace X17
 {
@@ -12,5 +13,8 @@ namespace X17
     #pragma link C++ class X17::MapPoint+;
     #pragma link C++ class X17::EndPoint+;
     #pragma link C++ class X17::Vector+;
+    #pragma link C++ class X17::DataPoint+;
+    #pragma link C++ class X17::TrackRK+;
+    #pragma link C++ class X17::StartPoint+;
     #endif
 } // namespace X17
