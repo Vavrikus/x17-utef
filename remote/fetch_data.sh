f23786cce912bbdf5d31fd7b8b51df75391137dd
Vavrikus <martinvitvavrik@gmail.com>
2023-11-02 21:35:16 +0100

    Added first somewhat successful grid-like simulation results.

--- remote/fetch_data.sh
+++ remote/fetch_data.sh
@@ -6,4 +6,4 @@ i="meta"
 echo "Connecting to ${i}"
 
 rsync ${i}:/storage/projects/utefx17/martin/data/ion_map/new_sample/* ../data/ion_map/new_sample/ --progress
-rsync ${i}:/storage/projects/utefx17/martin/data/micro_tracks/new_tracks/* ../data/micro_tracks/new_tracks/ --progress
+rsync ${i}:/storage/projects/utefx17/martin/data/micro_tracks/new_tracks/* ../data/micro_tracks/new_tracks/ --progress --recursive
